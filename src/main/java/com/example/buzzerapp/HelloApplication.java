97b447dba75f27d9ace33f9b02597871ce399a84
Noci96 <Albrecht.Nico96@outlook.com>
2024-11-12 13:49:16 +0100

    Changed screen-size to fullscreen

--- src/main/java/com/example/buzzerapp/HelloApplication.java
+++ src/main/java/com/example/buzzerapp/HelloApplication.java
@@ -11,9 +11,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), (1980*0.8), (1080*0.8));
         stage.setTitle("Hello!");
         stage.setScene(scene);
+
+        stage.setFullScreen(true);
+
         stage.show();
     }
 

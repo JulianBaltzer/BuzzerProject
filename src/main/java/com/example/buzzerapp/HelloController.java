473cce377af2b7107545365fa75e73c67e12a519
Julian <jbaltzer22@gmail.com>
2024-11-12 17:14:19 +0100

    Exit button added

--- src/main/java/com/example/buzzerapp/HelloController.java
+++ src/main/java/com/example/buzzerapp/HelloController.java
@@ -1,14 +1,32 @@
 package com.example.buzzerapp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class HelloController {
     @FXML
+    public Button closebutton;
+    @FXML
     private Label welcomeText;
+
+
+
+
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+
+    }
+
+    @FXML
+    protected void exit_app(){
+        Stage stage = (Stage) closebutton.getScene().getWindow();
+
+        stage.close();
     }
 }

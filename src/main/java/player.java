de1230b658b0f477e518bbb585046f039abe3e8c
Julian <jbaltzer22@gmail.com>
2024-11-12 10:11:22 +0100

    player klasse erstellt

---
+++ src/main/java/player.java
@@ -0,0 +1,17 @@
+public class player {
+
+    private String name;
+
+    public player(String name){
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
